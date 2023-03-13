748949c20871d4adb1bb2ae53d544b8c86f22989
DanielColeto <118129918+DanielColeto@users.noreply.github.com>
2023-03-13 20:40:44 +0100

    Update main.cpp

--- pruebaProyectoLightUp/pruebaProyectoLightUp/main.cpp
+++ pruebaProyectoLightUp/pruebaProyectoLightUp/main.cpp
@@ -11,6 +11,20 @@ int main() {
 
 	ifstream archivo;
 	tablero::operator>>(archivo, tablero);	
+
+	int i, j;
+	cout << BLUE<<"DONDE QUIERES PONER UNA BOMBILLA: " << "\n";
+
+	cin >> i >> j;
+	reglasJuego::ejecutarPos(tablero, i, j);
+		 
+		/*reglasJuego::ejecutarPos(tablero, 0, 0);
+		reglasJuego::ejecutarPos(tablero, 0, 2);
+		reglasJuego::ejecutarPos(tablero, 0, 4);
+		reglasJuego::ejecutarPos(tablero, 1, 3);
+		reglasJuego::ejecutarPos(tablero, 2, 0);
+		reglasJuego::ejecutarPos(tablero, 3, 1);
+		reglasJuego::ejecutarPos(tablero, 4, 3);*/
 	tablero::operator<<(cout,tablero);
 	
 	
