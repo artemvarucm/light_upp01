b5d413cf72a8a59a7855ad26a62207bf024fe9f1
artemvarucm <118186855+artemvarucm@users.noreply.github.com>
2023-03-13 10:05:33 +0100

    Create reglasJuego.h

---
+++ ConsoleApplication2/reglasJuego.h
@@ -0,0 +1,10 @@
+
+#include "tablero.h"
+
+namespace reglasJuego {
+	bool estaTerminado(const tTablero &juego);
+
+	bool esPosQuit(int x, int y);
+
+	void ejecutarPos(tTablero& tab, int x, int y);
+}
