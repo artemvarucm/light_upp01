7b1e02e9bd1fae45105657b648dc92f139fd5509
DanielColeto <118129918+DanielColeto@users.noreply.github.com>
2023-03-13 20:40:16 +0100

    Update tablero.cpp

--- pruebaProyectoLightUp/pruebaProyectoLightUp/tablero.cpp
+++ pruebaProyectoLightUp/pruebaProyectoLightUp/tablero.cpp
@@ -1,5 +1,5 @@
 #include "tablero.h"
-#include "colors.h"
+
 
 const int HUECOS = 2;
 const int N = 5;
@@ -14,7 +14,7 @@ void mostrarCoutSeparadorMat(int N) {
 
 void mostrarCabecera(int N) {
 	// muestra los numeros de columna
-	cout << "\t  |";
+	cout << WHITE << "\t  |";
 	for (int col = 0; col < N; ++col) {
 		cout << LBLUE << setw(HUECOS) << col << " " << RESET << '|';
 	}
@@ -30,11 +30,11 @@ void mostrarFila(const tTablero& tab, int i, int N) {
 	cout << "\t" << LBLUE << setw(HUECOS) << i << RESET << '|';
 	for (int c = 0; c < N; c++) {
 		char cel;
-		if (tab.datos[i][c].tipo == LIBRE) {
-			cout << BG_WHITE;
-		}
-		else if (tab.datos[i][c].tipo == BOMBILLA) {
+		if ((tab.datos[i][c].tipo == BOMBILLA) || (tab.datos[i][c].tipo == LIBRE && tab.datos[i][c].numBombillas > 0 && tab.datos[i][c].numBombillas < 5)) {
 			cout << BG_YELLOW << BLACK;
+		}
+		else if ((tab.datos[i][c].tipo == LIBRE) && (tab.datos[i][c].numBombillas <= 0 || tab.datos[i][c].numBombillas >= 5)) {
+			cout << BG_WHITE;
 		}
 		else if ((tab.datos[i][c].tipo == PARED) && (tab.datos[i][c].numBombillas >= 0 && tab.datos[i][c].numBombillas < 5)) {
 			cout << BG_BLACK << WHITE;
@@ -83,24 +83,26 @@ void tablero::ponCeldaEnPos(tTablero& tablero, int x, int y, const tCelda& c) {
 
 
 ifstream& tablero::operator>>(ifstream& archivo, tTablero& tab){
+	archivo.open("tablero.txt");
 	
-
-	archivo >> tab.nFils >> tab.nCols;
-	char tmp;
-	for (int i = 0; i < tab.nFils; i++) {
-		for (int j = 0; j < tab.nCols; j++) {
-			archivo >> tmp;
-			tablero::ponCeldaEnPos(tab,i, j,celda::charToCelda(tmp));
+	if (archivo.is_open()) {
+		archivo >> tab.nFils >> tab.nCols;
+		char tmp;
+		for (int i = 0; i < tab.nFils; i++) {
+			for (int j = 0; j < tab.nCols; j++) {
+				archivo >> tmp;
+				tablero::ponCeldaEnPos(tab,i, j,celda::charToCelda(tmp));
+			}
 		}
+		int numBombillas, pos_i, pos_j;
+		//tCelda celda;
+		//celda::ponBombilla(celda);
+		archivo >> numBombillas;
+		for (int k = 0; k < numBombillas; k++) {
+			archivo >> pos_i >> pos_j;
+			reglasJuego::ejecutarPos(tab, pos_i, pos_j);
+		}		
 	}
-	int numBombillas, pos_i, pos_j;
-	tCelda celda;
-	celda::ponBombilla(celda);
-	archivo >> numBombillas;
-	for (int k = 0; k < numBombillas; k++) {
-		archivo >> pos_i >> pos_j;
-		tablero::ponCeldaEnPos(tab, pos_i, pos_j, celda);
-	}		
 	
 	return archivo;
 }
@@ -111,3 +113,118 @@ ostream& tablero::operator<<(ostream& out, const tTablero& tab) {
 	return out;
 
 }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+
+bool reglasJuego::esPosQuit(int x, int y) {
+	bool ok = false;
+	if (x == -1 && y == 0) {
+		ok = true;
+	}
+	return ok;
+}
+const int i_offsets[4] = { 0,-1,0,1 };
+const int j_offsets[4] = { 1,0,-1,0 };
+
+void apagarCeldas(tTablero& tab, int x, int y) {
+
+	bool paradaDireccion = false;
+	for (int k = 0; k < 4; k++) {
+		int i = x;
+		paradaDireccion = false;
+		int j = y;
+		while (i < tablero::getNumFilas(tab) && i >= 0 && j < tablero::getNumCols(tab) && j >= 0 && !paradaDireccion) {
+			tCelda celda = tablero::celdaEnPos(tab, i, j);
+			if (celda::esPared(celda)) {
+				paradaDireccion = true;
+			}
+			else if (celda::estaIluminada(celda)) {
+				celda.numBombillas--;
+				tablero::ponCeldaEnPos(tab, i, j, celda);
+			}
+			j += j_offsets[k];
+			i += i_offsets[k];
+		}
+
+
+	}
+}
+
+void iluminarCeldas(tTablero& tab, int x, int y) {
+
+	bool paradaDireccion = false;
+	for (int k = 0; k < 4; k++) {
+		int i = x;
+		paradaDireccion = false;
+		//while ( && !paradaDireccion) {
+		int j = y;
+		while (i < tablero::getNumFilas(tab) && i >= 0 && j < tablero::getNumCols(tab) && j >= 0 && !paradaDireccion) {
+			tCelda celda = tablero::celdaEnPos(tab, i, j);
+			if (celda::esPared(celda)) {
+				paradaDireccion = true;
+			}
+			else if (celda.tipo == LIBRE) {
+				celda::iluminaCelda(celda);
+				tablero::ponCeldaEnPos(tab, i, j, celda);
+			}
+
+			j += j_offsets[k];
+			i += i_offsets[k];
+		}
+
+		//}
+	}
+}
+
+void reglasJuego::ejecutarPos(tTablero& tab, int x, int y) {
+	// validar no se sale tamanio
+	if (!esPosQuit(x, y)) {
+		tCelda celda = tablero::celdaEnPos(tab, x, y);
+		if (celda::estaIluminada(celda)) {
+			cout << "LA CELDA YA ESTA ILUMINADA, NO SE PUEDE PONER UNA BOMBILLA" << "\n";
+		}
+		else if (celda::esPared(celda)) {
+			cout << "LA CELDA ES UNA PARED" << "\n";
+		}
+		else {
+			if (celda::esBombilla(celda)) {
+				celda::apagaCelda(celda);
+				tablero::ponCeldaEnPos(tab, x, y, celda);
+				apagarCeldas(tab, x, y);
+			}
+			else if (celda::estaApagada(celda)) {
+				celda::ponBombilla(celda);
+				tablero::ponCeldaEnPos(tab, x, y, celda);
+				iluminarCeldas(tab, x, y);
+
+			}
+		}
+
+	}
+
+}
+/*
+bool reglasJuego::estaTerminado(const tTablero& juego) {
+
+	
+
+
+
+
+
+
+}
+
+*/
