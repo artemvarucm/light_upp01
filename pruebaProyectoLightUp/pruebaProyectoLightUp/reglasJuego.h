e75d87b53015d45e2e87cc911045f882622f9368
DanielColeto <118129918+DanielColeto@users.noreply.github.com>
2023-03-13 20:41:36 +0100

    Create reglasJuego.h

---
+++ pruebaProyectoLightUp/pruebaProyectoLightUp/reglasJuego.h
@@ -0,0 +1,17 @@
+/*
+#ifndef REGH
+#define REGH
+#include "tablero.h"
+
+namespace reglasJuego {
+	//bool estaTerminado(const tTablero& juego);
+
+	//bool esPosQuit(int x, int y);
+
+	void ejecutarPos(tTablero& tab, int x, int y);
+}
+
+#endif
+
+
+*/
