c17544527563de750d6194a18d0e61a8afeaa880
artemvarucm <118186855+artemvarucm@users.noreply.github.com>
2023-03-13 10:48:01 +0100

    Update reglasJuego.h

--- ConsoleApplication2/reglasJuego.h
+++ ConsoleApplication2/reglasJuego.h
@@ -1,10 +1,13 @@
-
+#ifndef REGH
+#define REGH
 #include "tablero.h"
 
 namespace reglasJuego {
-	bool estaTerminado(const tTablero &juego);
+	bool estaTerminado(const tTablero& juego);
 
 	bool esPosQuit(int x, int y);
 
 	void ejecutarPos(tTablero& tab, int x, int y);
 }
+
+#endif
