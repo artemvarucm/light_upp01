7d869a59949ffed6c9e09548fbee0e8b46454f4d
artemvarucm <118186855+artemvarucm@users.noreply.github.com>
2023-03-13 10:47:41 +0100

    Update reglasJuego.cpp

--- ConsoleApplication2/reglasJuego.cpp
+++ ConsoleApplication2/reglasJuego.cpp
@@ -2,9 +2,9 @@
 
 
 bool reglasJuego::esPosQuit(int x, int y) {
-	bool ok = true;;
+	bool ok = false;
 	if (x == -1 && y == 0) {
-		ok = false;
+		ok = true;
 	}
 	return ok;
 }
@@ -17,21 +17,21 @@ void apagarCeldas(tTablero& tab, int x, int y) {
 	for (int k = 0; k < 4; k++) {
 		int i = x;
 		paradaDireccion = false;
-		while (i < tablero::getNumFilas(tab) && i >=0 && !paradaDireccion) {
-			int j = y;
-			while (j < tablero::getNumCols(tab) && j >= 0 && !paradaDireccion) {
-				tCelda celda = tablero::celdaEnPos(tab, x, y);
-				if (celda::esPared(celda)) {
-					paradaDireccion = true;
-				}
-				else if (celda::estaIluminada(celda)) {
-						celda.numBombillas--;
-						tablero::ponCeldaEnPos(tab, x, y, celda);
-				}
-				j += j_offsets[k];
+		int j = y;
+		while (i < tablero::getNumFilas(tab) && i >= 0 && j < tablero::getNumCols(tab) && j >= 0 && !paradaDireccion) {
+			tCelda celda = tablero::celdaEnPos(tab, i, j);
+			if (celda::esPared(celda)) {
+				paradaDireccion = true;
 			}
+			else if (celda::estaIluminada(celda)) {
+				celda.numBombillas--;
+				tablero::ponCeldaEnPos(tab, i, j, celda);
+			}
+			j += j_offsets[k];
 			i += i_offsets[k];
 		}
+			
+		
 	}
 }
 
@@ -41,37 +41,42 @@ void iluminarCeldas(tTablero& tab, int x, int y) {
 	for (int k = 0; k < 4; k++) {
 		int i = x;
 		paradaDireccion = false;
-		while (i < tablero::getNumFilas(tab) && i >= 0 && !paradaDireccion) {
+		//while ( && !paradaDireccion) {
 			int j = y;
-			while (j < tablero::getNumCols(tab) && j >= 0 && !paradaDireccion) {
-				tCelda celda = tablero::celdaEnPos(tab, x, y);
-				if (celda::esPared(celda)) {					
+			while (i < tablero::getNumFilas(tab) && i >= 0 && j < tablero::getNumCols(tab) && j >= 0 && !paradaDireccion) {
+				tCelda celda = tablero::celdaEnPos(tab, i, j);
+				if (celda::esPared(celda)) {
 					paradaDireccion = true;
 				}
-				else {
+				else if(celda.tipo == LIBRE){
 					celda::iluminaCelda(celda);
+					tablero::ponCeldaEnPos(tab, i, j, celda);
 				}
-				
+
 				j += j_offsets[k];
+				i += i_offsets[k];
 			}
-			i += i_offsets[k];
-		}
+			
+		//}
 	}
 }
 
 void reglasJuego::ejecutarPos(tTablero& tab, int x, int y) {
+	// validar no se sale tamanio
 	if (!esPosQuit(x, y)) {
 		tCelda celda = tablero::celdaEnPos(tab, x, y);
 		if (celda::esBombilla(celda)) {
 			celda::apagaCelda(celda);
+			tablero::ponCeldaEnPos(tab, x, y, celda);
 			apagarCeldas(tab, x, y);
 		}
 		else if (celda::estaApagada(celda)) {
 			celda::ponBombilla(celda);
+			tablero::ponCeldaEnPos(tab, x, y, celda);
 			iluminarCeldas(tab, x, y);
-		}
-		
-	}
-	
-}
 
+		}
+
+	}
+
+}
