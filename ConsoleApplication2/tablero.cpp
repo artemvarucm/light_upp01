1eaff1af2eface4db376bceaf720bab2cc55e14e
DanielColeto <118129918+DanielColeto@users.noreply.github.com>
2023-03-27 15:39:55 +0200

    hacer que no dependa de N

--- ConsoleApplication2/tablero.cpp
+++ ConsoleApplication2/tablero.cpp
@@ -1,5 +1,5 @@
 #include "tablero.h"
-#include "colors.h"
+
 #include "reglasJuego.h"
 // cambiar los includes
 
@@ -16,65 +16,81 @@ tCelda tablero::celdaEnPos(const tTablero& tablero, int x, int y) {
 }
 
 void tablero::ponCeldaEnPos(tTablero& tablero, int x, int y, const tCelda& c) {
-	if (x >= 0 && y >= 0 && x < tablero.nFils && y < tablero.nCols ){
+	if (x >= 0 && y >= 0 && x < tablero.nFils && y < tablero.nCols) {
 		tablero.datos[x][y] = c;
 	}
 }
 
 // FIX
-const int HUECO = 2;
-
-void mostrarHeader(const tTablero& tab) {
-	cout << "\t  ";
-	for (int col = 0; col < tab.nCols; ++col) {
-		cout << "|" << setw(HUECO + 1) << setfill(' ') << col;
-		cout << setw(HUECO + 1) << setfill(' ');
-	}
-	cout << "|";
-	cout << endl;
-}
+const int HUECOS = 2;
 
 void mostrarCoutSeparadorMat(const tTablero& tab) {
 	cout << "\t -+";
 	for (int col = 0; col < tab.nCols; ++col) {
-		cout << setw(HUECO*2 + 2) << setfill('-') << '+';
+		cout << setw(HUECOS + 2) << setfill('-') << '+' << setfill(' ');
 	}
 	cout << endl;
 }
 
-void mostrarFila(int i, const tTablero& tab) {
-	cout << "\t " << i;
+void mostrarCabecera(const tTablero& tab) {
+	// muestra los numeros de columna
+	cout << WHITE << "\t  |";
 	for (int col = 0; col < tab.nCols; ++col) {
-		cout << RESET << "|";
-		
-		tCelda celda = tablero::celdaEnPos(tab, i, col);
-		if (celda.tipo == BOMBILLA || (celda.tipo == LIBRE && celda.numBombillas > 0)) {
-			cout << BG_YELLOW;
-		} else if (celda.tipo == LIBRE) {
+		cout << LBLUE << setw(HUECOS) << col << " " << RESET << '|';
+	}
+	cout << endl;
+
+	// muestra la linea separadora 
+	mostrarCoutSeparadorMat(tab);
+}
+
+void mostrarFila(const tTablero& tab, int i) {
+
+	//muestra una fila donde todas las celdas son bombillas 
+	cout << "\t" << LBLUE << setw(HUECOS) << i << RESET << '|';
+	for (int c = 0; c < tab.nCols; c++) {
+		char cel;
+		if ((tab.datos[i][c].tipo == BOMBILLA) || (tab.datos[i][c].tipo == LIBRE && tab.datos[i][c].numBombillas > 0 && tab.datos[i][c].numBombillas < 5)) {
+			cout << BG_YELLOW << BLACK;
+		}
+		else if ((tab.datos[i][c].tipo == LIBRE) && (tab.datos[i][c].numBombillas <= 0 || tab.datos[i][c].numBombillas >= 5)) {
 			cout << BG_WHITE;
 		}
-	    cout << setw(HUECO + 1) << setfill(' ');
-		cout << celda::celdaToChar(celda);
-		cout << setw(HUECO + 4) << setfill(' ');
+		else if ((tab.datos[i][c].tipo == PARED) && (tab.datos[i][c].numBombillas >= 0 && tab.datos[i][c].numBombillas < 5)) {
+			cout << BG_BLACK << WHITE;
+		}
+		else {
+			cout << BG_BLACK;
+		}
+
+		cel = celda::celdaToChar(tab.datos[i][c]);
+		cout << setw(HUECOS) << setfill(' ') << cel << " " << RESET;
+		cout << '|';
 	}
-	cout << RESET << "|";
 	cout << endl;
+
+	// muestra la linea separadora 
 	mostrarCoutSeparadorMat(tab);
+}
+
+void mostrarTablero(const tTablero& tab) {
+	for (int l = 0; l < tab.nFils; l++) {
+		mostrarFila(tab, l);
+	}
+
 }
 
 ostream& tablero::operator<<(ostream& out, const tTablero& tab) {
-	mostrarHeader(tab);
-	mostrarCoutSeparadorMat(tab);
-	for (int i = 0; i < tab.nFils; i++) {
-		mostrarFila(i, tab);
-	}
+	mostrarCabecera(tab);
+	mostrarTablero(tab);
 	return out;
+
 }
 
 ifstream& tablero::operator>>(ifstream& archivo, tTablero& tab) {
 	archivo >> tab.nFils >> tab.nCols;
 	for (int i = 0; i < tab.nFils; i++) {
-		for (int j = 0; j < tab.nCols; j++){
+		for (int j = 0; j < tab.nCols; j++) {
 			char c;
 			archivo >> c;
 			ponCeldaEnPos(tab, i, j, celda::charToCelda(c));
