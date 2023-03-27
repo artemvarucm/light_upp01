0062e27c1c7345f9d41da90f2df23be5aea55cc5
DanielColeto <118129918+DanielColeto@users.noreply.github.com>
2023-03-27 15:31:47 +0200

    Update main.cpp

--- pruebaProyectoLightUp/pruebaProyectoLightUp/main.cpp
+++ pruebaProyectoLightUp/pruebaProyectoLightUp/main.cpp
@@ -1,63 +1,63 @@
+
 #include "reglasJuego.h"
 #include <fstream>
-using namespace tablero;
+	using namespace tablero;
 
-int main() {
+	int main() {
 
-	tTablero tablero;
-	
-	ifstream archivo;
-	archivo.open("./tableros.txt");
-	if (archivo.is_open()) {
-		archivo >> tablero;
-		archivo.close();
-	}
-	cout << WHITE<< "\n" << "\n" <<"\t" << "     " << "JUEGO LIGHT-UP" << "\n" << "\n";
+		tTablero tablero;
 
-	cout << tablero;
-	
-	int contadorJuego = 0;
-	bool bien = true;
-	int x, y;
-	while(!reglasJuego::estaTerminado(tablero) && bien) {
+		ifstream archivo;
+		archivo.open("./tablero.txt");
+		if (archivo.is_open()) {
+			archivo >> tablero;
+			archivo.close();
+		}
+		cout << WHITE << "\n" << "\n" << "\t" << "     " << "JUEGO LIGHT-UP" << "\n" << "\n";
+
+		cout << tablero;
+
+		int contadorJuego = 0;
+		bool bien = true;
 		
-		cout << WHITE << "INTODUCE DONDE QUIERES COLOCAR/QUITAR UNA BOMBILLA" << "\n";
+		while (!reglasJuego::estaTerminado(tablero) && bien) {
+			int x;
+			int y;
+			cout << WHITE << "INTODUCE DONDE QUIERES COLOCAR/QUITAR UNA BOMBILLA" << "\n";
 
-		
-		cout << WHITE << "Posicion x: ";
-		cin >> x;
-		cout << WHITE << "Posicion y: ";
-		cin >> y;
-	
-		if (!reglasJuego::esPosQuit(x, y)) {
-			
-			reglasJuego::ejecutarPos(tablero, x, y);
-			cout << tablero;
-			contadorJuego++;
-			cout << "Contador: " << contadorJuego << "\n";
+
+			cout << WHITE << "Posicion x: ";
+			cin >> x;
+			cout << WHITE << "Posicion y: ";
+			cin >> y;
+
+			if (!reglasJuego::esPosQuit(x, y)) {
+
+				reglasJuego::ejecutarPos(tablero, x, y);
+				cout << tablero;
+				contadorJuego++;
+				cout << "Contador: " << contadorJuego << "\n";
+
+
+
+			}
+			else {
+				bien = false;
+				cout << WHITE << "El juego ha acabado, has decidido abandonar." << "\n";
+			}
+
+
+			cout << "\n";
 
 
 
 		}
-		else {
-			bien = false;
+		if (reglasJuego::estaTerminado(tablero)) {
+			cout << WHITE << "HAS GANADO!!!" << "\n";
+			cout << WHITE << "Has terminado el juego en " << contadorJuego << " movimientos" << "\n";
 		}
 
 
-		cout << "\n";
 
-		
-
+		return 0;
 	}
-	if (reglasJuego::esPosQuit(x, y)) {
-		cout << WHITE << "El juego ha acabado, has decidido abandonar." << "\n";
-	}
-	else if (reglasJuego::estaTerminado(tablero)) {
-		cout << WHITE << "HAS GANADO!!!" << "\n";
-		cout << WHITE << "Has terminado el juego en "<<contadorJuego << " movimientos"<< "\n";
-	}
-
-
-	
-	return 0;
-}
