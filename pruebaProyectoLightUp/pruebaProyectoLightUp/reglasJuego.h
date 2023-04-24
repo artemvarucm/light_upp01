4b5fb5181d472979229e9135085c19e1f87f0f27
artemvarucm <118186855+artemvarucm@users.noreply.github.com>
2023-04-24 10:40:53 +0200

    reset tablero

--- pruebaProyectoLightUp/pruebaProyectoLightUp/reglasJuego.h
+++ pruebaProyectoLightUp/pruebaProyectoLightUp/reglasJuego.h
@@ -4,10 +4,11 @@
 
 namespace reglasJuego {
 	bool estaTerminado(const tTablero& juego);
-
 	bool esPosQuit(int x, int y);
-
+	bool esPosReset(int x, int y);
 	void ejecutarPos(tTablero& tab, int x, int y);
+	void resetTablero(tTablero& tab);
+
 }
 
 #endif
